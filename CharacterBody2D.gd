97ec96bd9c8414f5da13174e756afc78df41e952
julianzotelo <juli.zotelo@gmail.com>
2024-05-16 17:29:54 -0300

    salto

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const MoveSpeed = 250
 const MaxSpeed = 50
-const JUMP_VELOCITY = -40
+const JUMP_VELOCITY = -400
 
 const up = Vector2(0,-1)
 #const gravity = 15
@@ -12,9 +12,31 @@ const up = Vector2(0,-1)
 var motion = Vector2()
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+#func _physics_process(delta):
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		#$AnimationPlayer.play("Flip")
+	#else:
+		#$AnimationPlayer.play("idle")
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * MoveSpeed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, MoveSpeed)
+#
+	#move_and_slide()
 func _physics_process(delta):
 
-	motion.y += gravity
+	motion.y -= gravity
 	##var friction = false
 	#
 	#if Input.is_action_pressed("ui_right"):
@@ -33,7 +55,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") :
 		velocity.y = JUMP_VELOCITY
-	if is_on_floor():
+	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
